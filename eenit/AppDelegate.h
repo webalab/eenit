aa61919827b102dcfe50a3eae6b57e29c7ff3898
Antonio Perez <antonio@hexperimental.com>
2012-01-28 17:01:24 -0800

    Initial Commit

---
+++ eenit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  eenit
+//
+//  Created by Antonio Perez Salas on 12-01-28.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ eenit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  eenit
+//
+//  Created by Antonio Perez Salas on 12-01-28.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
