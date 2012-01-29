6ae3008702338d525657e0832099c8d0c5581c8d
Antonio Perez <antonio@hexperimental.com>
2012-01-28 20:58:20 -0800

    Tweaks and  modifications to project

--- eenit/classes/utils/CDataHandler.h
+++ eenit/classes/utils/CDataHandler.h
@@ -13,7 +13,7 @@
 
 extern NSString * const dataModelID;
 
-@property (nonatomic, retain, readonly) NSString *dataModelID;
+//@property (nonatomic, retain, readonly) NSString *dataModelID;
 @property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
 @property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
 @property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
